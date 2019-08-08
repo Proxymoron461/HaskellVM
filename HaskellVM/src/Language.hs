0358c6fefbdc01926d0a90cf5f73a6b4fa4c7319
Proxymoron461 <tobyjonahbee@gmail.com>
2019-08-08 16:09:23 +0100

    Defined the language in a file.

---
+++ HaskellVM/src/Language.hs
@@ -0,0 +1,38 @@
+module Language where
+
+--------------------------------------------------------------------------------
+-- | Importing things goes here
+
+--------------------------------------------------------------------------------
+
+data Program = ZipList (Int, Stmt)
+
+data Stmt
+    = JumpTo { 
+        lineNo :: Int
+    }
+    | JumpToWhenZero {
+        lineNo :: Int,
+        location :: Var
+    }
+    | SetValueToVar {
+        destvar :: Var,
+        val :: Int
+    }
+    | SetVarToVar {
+        destvar :: Var,
+        supplyvar :: Var
+    }
+    | AddValueToVar {
+        destvar :: Var,
+        val :: Int
+    }
+    | AddVarToVar {
+        destvar :: Var,
+        supplyvar :: Var
+    }
+    | End
+
+data Var = Address Int | Name String | Value Int
+
+type OutputMemory = [(Int, Int)]
